a1292e4008df30f601418f7f073a90eedf675f48
dev <denadar@deloitte.com>
2023-08-12 12:30:42 +0530

    Initial Commit

---
+++ shared/src/androidMain/kotlin/com/weather/app/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.weather.app
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
